f69f3584d021c0b380c20e1a3f2585d3655f7b30
venomega <guilleps92@nauta.cu>
2021-06-13 19:01:38 -0400

    fix stdin buffer, added caracter count

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from curses import wrapper
+import curses
 import win
 import socket
 import threading
@@ -36,20 +37,55 @@ def the_reader():
             for i in token:
                 for x in i:
                     if not x == "\n":
+                        if len(buffer) >= memory.screen_width:
+                            memory.window.buffer_add(buffer)
+                            buffer = ""
                         buffer += x
             memory.window.buffer_add(buffer)
         if "PING" in recv:
             memory.object.send(f"PONG {recv.split()[-1]}\n".encode())
 
 
-def screen(stdscr):
-    global NICK, USER
-    memory.window = win.window(0.99, 0.99, 0.01, 0.01)
-    memory.window.build()
-    connect(NICK, USER)
+def screen_loop():
     while True:
         memory.window.write_buffer()
         time.sleep(1)
+
+
+def screen(stdscr):
+    global NICK, USER
+    memory.screen_width = curses.COLS - 2
+    memory.window = win.window(0.90, 0.90, 0.01, 0.01)
+    memory.window.build()
+    connect(NICK, USER)
+    threading.Thread(target=screen_loop).start()
+
+    o = win.window(0.1, 0.99, 0.9, 0.01)
+    o.build(1)
+    num = 200
+    buffer = ""
+    while True:
+        asd = o.win.getch()
+
+        if asd == 10:  # enter has pressed
+            if len(buffer) > 0:
+                memory.object.send(f"{buffer}\n".encode())
+                buffer = ""
+        if asd in [8, 127]:  # backspace
+            buffer = buffer[:-1]
+        if num - len(buffer) <= 0:
+            continue
+        if asd >= 97 and asd <= 122:  # lower keys
+            buffer += asd.to_bytes(1, 'little').decode()
+        if asd >= 65 and asd <= 90:  # upper keys
+            buffer += asd.to_bytes(1, 'little').decode()
+
+        o.write(0, 0, " " * (memory.screen_width))
+        if len(buffer) > memory.screen_width:
+            o.write(0, 0, buffer[eval(f"-{memory.screen_width - 1}"):])
+        else:
+            o.write(0, 0, buffer)
+        o.write(1, 0, f"{num-len(buffer)}")
 
 
 wrapper(screen)
